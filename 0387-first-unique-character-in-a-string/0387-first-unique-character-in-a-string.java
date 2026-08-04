278383f877a7fd2f97b9c2943813a35862c42d98
vtu30157 <vtu30157@veltech.edu.in>
2026-08-04 14:54:55 +0530

    Time: 31 ms (37.91%), Space: 47 MB (40.46%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+import java.util.HashMap;
+class Solution {
+public int firstUniqChar(String s) {
+    Map<Character, Integer> map = new
+    HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+        char ch = s.charAt(i);
+        map.put(ch, map.getOrDefault(ch, 0) + 1);
+}
+for (int i = 0; i < s.length(); i++) {
+if (map.get(s.charAt(i)) == 1) {
+return i;
+}
+}
+return -1;
+}
+public static void main(String[] args) {
+Solution s = new Solution();
+System.out.println(s.firstUniqChar("leetcode"));
+System.out.println(s.firstUniqChar("loveleetcode")); 
+System.out.println(s.firstUniqChar("aabb"));
+}
+}
